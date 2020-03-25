334484ba362b555b9a15ce8c572bda658d825d70
Jairo.Julia <jairocnx@gmail.com>
2020-03-25 12:32:57 +0100

    First Commit

---
+++ FirstTest/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola Jose y Javi espero que funcione esto!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
